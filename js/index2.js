d3781d66dbb14f4cba2a3b403a7f3e508bcb402e
anaelle-bargas <143993957+anaelle-bargas@users.noreply.github.com>
2023-12-20 13:19:53 +0100

    Update index2.js

--- js/index2.js
+++ js/index2.js
@@ -3,7 +3,7 @@ window.onload = function() {
     window.addEventListener('beforeunload', function() {
         // Élaborer la destination souhaitée, telle que l'U.R.L. de vos désirs
         var destination = window.location.href.split('#')[0];
-        conosle.log(destination);
+        console.log(destination);
         // Énoncer le décret pour conduire l'usager vers la destinée prédéfinie
         window.location.href = destination;
     });
